7cc723b630f03df78c4c5c68a740e316ed884adc
Atsu Vovor <atsu.vovor@gmail.com>
2025-12-20 01:45:53 -0500

    Update model_inference.py

--- model_inference/model_inference.py
+++ model_inference/model_inference.py
@@ -38,8 +38,6 @@ MODEL_NAME = "Stacked_AD_classifier"
 THREASHHOLD_PERC = 95
 LABEL_COL = "Threat Level"  # Ground truth label column name
 MODELS_DIR = "CyberThreat_Insight/stacked_models_deployment"
-#SIMULATED_REAL_TIME_DATA_FILE = \
-#                         "/content/drive/My Drive/Cybersecurity Data/normal_and_anomalous_cybersecurity_dataset_for_google_drive_kb.csv"
 url = "https://drive.google.com/file/d/1Nr9PymyvLfDh3qTfaeKNVbvLwt7lNX6l/view?usp=drive_link"
 
 # --------------------------
