e6c42df6a134739ff01b014ee6cd890d7356a1b5
Atsu Vovor <atsu.vovor@gmail.com>
2025-12-20 02:41:34 -0500

    Update stacked_anomaly_detection_classifier.py

--- model_dev/stacked_model/stacked_anomaly_detection_classifier.py
+++ model_dev/stacked_model/stacked_anomaly_detection_classifier.py
@@ -29,8 +29,6 @@ from sklearn.metrics import (accuracy_score, precision_score, recall_score,
                              average_precision_score, roc_auc_score)
 from sklearn.decomposition import PCA
 import tensorflow as tf
-#from google.colab import drive
-#drive.mount("/content/drive")
 from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.layers import Input, Dense, LSTM, RepeatVector
 from tensorflow.keras.callbacks import EarlyStopping
