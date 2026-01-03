17d64e755b1e1b1bfc7eab319ddebf5f4b828167
Atsu Vovor <atsu.vovor@gmail.com>
2026-01-02 21:31:51 -0500

    Create executive_agent.py

---
+++ ai_agent/executive_agent.py
@@ -0,0 +1,10 @@
+from llm.interface import LocalLLM
+from utils.cleaner import clean_output
+
+class ExecutiveAgent:
+    def __init__(self):
+        self.llm = LocalLLM()
+
+    def summarize(self, prompt):
+        text = self.llm.generate(prompt)
+        return clean_output(text)
